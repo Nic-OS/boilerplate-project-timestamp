2c6ad4a4d81395747dbc1285057569122ae96b64
Nico Salas <salasn24@gmail.com>
2019-08-09 16:43:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,11 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/timestamp/:date_string?', function (req, res) {
+  const date = new Date(req.params.date_string);
 
+  res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
